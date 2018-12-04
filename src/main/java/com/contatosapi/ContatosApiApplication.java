7c3c19a45ae7316c74b2f17e9e560aed0f040131
Natan Cardoso <natancb7@gmail.com>
2018-12-04 17:20:43 -0300

    Commit inicial - estrutura do projeto

---
+++ src/main/java/com/contatosapi/ContatosApiApplication.java
@@ -0,0 +1,12 @@
+package com.contatosapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContatosApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContatosApiApplication.class, args);
+	}
+}
