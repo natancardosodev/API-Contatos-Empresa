7ed8e930744d981e3f4a8ff11fbde42de3ea61c4
Natan Cardoso <natancb7@gmail.com>
2018-12-04 17:59:03 -0300

    Camada Repository

---
+++ src/main/java/com/contatosapi/repository/ClienteRepository.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package com.contatosapi.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.contatosapi.model.Cliente;
+
+/**
+ * @author natancardosodev
+ *
+ */
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente, Long> {
+	
+	public Cliente findByNome(String nome);
+	
+	public Cliente findByEmail(String email);
+	
+	public void deleteById (Long id);
+
+}
---
+++ src/main/java/com/contatosapi/repository/FornecedorRepository.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package com.contatosapi.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.contatosapi.model.Fornecedor;
+
+/**
+ * @author natancardosodev
+ *
+ */
+@Repository
+public interface FornecedorRepository extends JpaRepository<Fornecedor, Long> {
+	
+	public Fornecedor findByNome(String nome);
+	
+	public Fornecedor findByEmail(String email);
+	
+	public void deleteById (Long id);
+
+}
